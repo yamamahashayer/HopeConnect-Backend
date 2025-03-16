576e378507151c3a4259e06945e66b9f18fd9300
Tasneem-jawabrah <s12113501@stu.najah.edu>
2025-03-16 20:20:20 +0200

    Initial commit

---
+++ HopeConnect/HopeConnect/src/main/java/com/example/HopeConnect/Conrollers/UserController.java
@@ -0,0 +1,44 @@
+package com.example.HopeConnect.Conrollers;
+
+
+import com.example.HopeConnect.Models.Entity.User;
+import com.example.HopeConnect.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserServices userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
