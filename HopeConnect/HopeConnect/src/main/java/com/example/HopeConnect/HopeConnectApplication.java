a454800f1efec22498acb6cab163a38136187fb6
DialaEShami <shamidyala@gmail.com>
2025-03-16 20:26:05 +0200

    sec commit

--- HopeConnect/HopeConnect/src/main/java/com/example/HopeConnect/HopeConnectApplication.java
+++ HopeConnect/HopeConnect/src/main/java/com/example/HopeConnect/HopeConnectApplication.java
@@ -8,7 +8,7 @@ public class HopeConnectApplication {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!");
+		System.out.println("Hell world!");
 		SpringApplication.run(HopeConnectApplication.class, args);
 	}
 }
