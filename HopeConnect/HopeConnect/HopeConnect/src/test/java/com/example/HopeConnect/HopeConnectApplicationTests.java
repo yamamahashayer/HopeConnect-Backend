f9245eaaf3a468205c19ad3ce282fb982bf9d5ba
Tasneem-jawabrah <s12113501@stu.najah.edu>
2025-03-26 02:12:27 +0200

    Restored files from commit 6b7885d

---
+++ HopeConnect/HopeConnect/HopeConnect/src/test/java/com/example/HopeConnect/HopeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HopeConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ HopeConnect/README.md
@@ -0,0 +1,2 @@
+# HopeConnect-Backend
+ A RESTful API built with Spring Boot and MySQL to support orphaned children in Gaza. Enables donations, sponsorships, and volunteer services, ensuring transparency and efficiency.   
