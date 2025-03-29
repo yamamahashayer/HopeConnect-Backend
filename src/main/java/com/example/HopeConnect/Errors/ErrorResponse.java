6b34298068fa1f5ebd85ca43c215676873cb22be
yamamah ashayer <yamamahashayer@gmail.com>
2025-03-29 03:33:22 -0700

    Volunteer files

--- src/main/java/com/example/HopeConnect/Errors/ErrorResponse.java
+++ src/main/java/com/example/HopeConnect/Errors/ErrorResponse.java
@@ -2,10 +2,17 @@ package com.example.HopeConnect.Errors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-@Getter
-@Setter
-@AllArgsConstructor
+
+
 public class ErrorResponse {
     private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
+
 
