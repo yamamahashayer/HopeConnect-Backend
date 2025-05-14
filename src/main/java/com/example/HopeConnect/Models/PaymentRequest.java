86ed3346ea6ead82c97990b1a25cbe7c0fe93653
engta <s12113501@stu.najah.edu>
2025-05-14 09:30:13 +0300

    payment update

--- src/main/java/com/example/HopeConnect/Models/PaymentRequest.java
+++ src/main/java/com/example/HopeConnect/Models/PaymentRequest.java
@@ -1,6 +1,8 @@
 package com.example.HopeConnect.Models;
 public class PaymentRequest {
     private long amount;
+    private Long sponsorActivityId;
+
 
     public long getAmount() {
         return amount;
@@ -8,5 +10,14 @@ public class PaymentRequest {
 
     public void setAmount(long amount) {
         this.amount = amount;
+
+    }
+
+    public Long getSponsorActivityId() {
+        return sponsorActivityId;
+    }
+
+    public void setSponsorActivityId(Long sponsorActivityId) {
+        this.sponsorActivityId = sponsorActivityId;
     }
 }
